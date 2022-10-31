418142b2589b1f9dfe351cc34d4d131fdb093661
Karinny Lemos <99560287+karinnylemosb@users.noreply.github.com>
2022-10-31 20:47:17 -0300

    Aprendendo sobre Reactive Forms

---
+++ Reactive-forms/angular-reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss']
+})
+export class ReactiveFormsComponent implements OnInit {
+
+  public cadastroForm: FormGroup = this.formBuilder.group({
+    firstName:['', Validators.required],
+    lastName:[''],
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  public submitForm() {
+    if(this.cadastroForm.valid){
+      console.log(this.cadastroForm.value.firstName);
+      console.log(this.cadastroForm.value.lastName);
+    }
+
+  }
+
+}
+
+//O formGroup é um tipo de grupo de formulários.
